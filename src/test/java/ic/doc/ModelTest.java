a0b23e7c338c7202b1a7c24a681355c1cad62664
Katherine Miles <kam20@texel22.doc.ic.ac.uk>
2022-03-01 16:51:31 +0000

    K&G: added tests for adding, subtracting, mult and dividing

--- src/test/java/ic/doc/ModelTest.java
+++ src/test/java/ic/doc/ModelTest.java
@@ -4,13 +4,15 @@ import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Test;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+
 public class ModelTest {
 
     public JUnitRuleMockery context = new JUnitRuleMockery();
     Model model = new Model();
-
     View viewMock = context.mock(View.class);
-
 
     @Test
     public void doesNotCalculateIfStackIsSmallerThanTwo(){
@@ -22,5 +24,62 @@ public class ModelTest {
         model.findResult("+");
 
     }
+
+    @Test
+    public void canSetView() {
+        GuiApp app = new GuiApp();
+        model.setView(app);
+    }
+
+    @Test
+    public void canAddTwoNumbers(){
+        GuiApp app = new GuiApp();
+        model.setView(app);
+        model.addNumberToStack(4);
+        model.addNumberToStack(3);
+        model.findResult("+");
+
+        assertThat(String.valueOf(model.numberList.peek()), is("7"));
+
+    }
+
+    @Test
+    public void canMultiplyTwoNumbers(){
+        GuiApp app = new GuiApp();
+        model.setView(app);
+        model.addNumberToStack(4);
+        model.addNumberToStack(3);
+        model.findResult("*");
+
+        assertThat(String.valueOf(model.numberList.peek()), is("12"));
+
+    }
+
+    @Test
+    public void canSubtractTwoNumbers(){
+        GuiApp app = new GuiApp();
+        model.setView(app);
+        model.addNumberToStack(4);
+        model.addNumberToStack(3);
+        model.findResult("-");
+
+        assertThat(String.valueOf(model.numberList.peek()), is("1"));
+
+    }
+
+
+    @Test
+    public void canDivideTwoNumbers(){
+        GuiApp app = new GuiApp();
+        model.setView(app);
+        model.addNumberToStack(4);
+        model.addNumberToStack(2);
+        model.findResult("/");
+
+        assertThat(String.valueOf(model.numberList.peek()), is("2"));
+
+    }
+
+
 
 }
